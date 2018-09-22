393be8edaf7d505859c3b78841c7812926587f8f
imjanellarazel <janellarazelcalderon14@gmail.com>
2018-09-22 11:33:36 +0800

    Initial commit

---
+++ app/src/main/java/com/calderon/janella/constraintlayout.java
@@ -0,0 +1,13 @@
+package com.calderon.janella;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class constraintlayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_constraintlayout);
+    }
+}
